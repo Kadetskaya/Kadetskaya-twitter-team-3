2c6471ed6eb7e660a85d697615eb71897562b6d9
Yuschyk <yuschyky@gmail.com>
2020-03-31 17:35:46 +0300

    add tabs and new selectors

--- public/js/personal.js
+++ public/js/personal.js
@@ -1,10 +1,11 @@
 (function($) {
     $(function() {
 
-        $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $('ul.personal-tabs__caption').on('click', 'li:not(.active)', function() {
             $(this)
                 .addClass('active').siblings().removeClass('active')
-                .closest('div').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+                .closest('div')
+            $(".tabs-content-block").find('div.personal-tabs__content').removeClass('active').eq($(this).index()).addClass('active');
         });
 
     });
